6ac48970d360843a3072cef02fa49b57eff597da
Marc Cruz <fcpmarcus@gmail.com>
2015-02-02 08:58:16 -0800

    updated js

--- tttjs.js
+++ tttjs.js
@@ -1,20 +1,18 @@
-var player1 = "X"
+var player1 = "X";
 
 //Game logic----------------------------------------------------------------------//
+
+// Makes sure there is no winner before the game starts
 function startGame(){
-	document.winner = null;						// Makes sure there is no winner before the game starts
+	document.winner = null;						
 	setMessage(player1 + " gets to start!");
 };
-
+// Creates messages for who's turn it is or whoever won.
+// Displayed in html
 function setMessage(msg){
 	document.getElementById('gameMessage').innerHTML = msg;
-}; // Creates messages for winner or who's turn it is displayed on html
+}; 
 
-
-function getBox (number) {
-	return document.getElementById("cell"+number).innerHTML;
-
-};
 
 //Determine whose turn it is.
 function switchTurn(){
@@ -28,10 +26,41 @@ function switchTurn(){
 			player1 = "X"
 			setMessage(player1 + "'s turn to pick!");
 			}
+};
+
+//This determines if the move has already resulted in the person winning
+//or if a cell has already been occupied.
+function playerMove(cell) {
+	if (document.winner != null) {
+		setMessage(document.winner + " has already won the game!");
+	}	else if (cell.innerHTML == ""){
+		cell.innerHTML = player1;
+		switchTurn();
+	} 	else {
+			setMessage("Position already taken.");
+			}
 
 };
 
 //Winning combinations
+
+//Determines which cell number is clicked so that it can be used for the
+//checkRow function
+function getBox (number) {
+	return document.getElementById("cell"+number).innerHTML;
+
+};
+
+//Check to see if the moves have resulted in 3 in a row.
+function checkRow (a, b, c, move) {
+	var result = false;
+	if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+		result = true;
+	};
+	return result;
+};
+
+//Determines if the row is one of the winning combinations
 function checkForWinner(move) {
 	var result = false;
 	if (checkRow(1, 2, 3, move) || checkRow(4, 5, 6, move) ||
@@ -45,22 +74,3 @@ function checkForWinner(move) {
 };
 
 
-function playerMove(cell) {
-	if (document.winner != null) {
-		setMessage(document.winner + " has already won the game!");
-	}	else if (cell.innerHTML == ""){
-		cell.innerHTML = player1;
-		switchTurn();
-	} 	else {
-			setMessage("Position already taken.");
-			}
-
-};
-
-function checkRow (a, b, c, move) {
-	var result = false;
-	if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
-		result = true;
-	};
-	return result;
-};
